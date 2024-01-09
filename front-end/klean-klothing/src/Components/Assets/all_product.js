b63ea367bc9cdd8a971a2b1fb5de4b1810943340
Oatile <osmdlela@gmail.com>
2024-01-09 22:37:00 +0200

    Initial Commit

---
+++ front-end/klean-klothing/src/Components/Assets/all_product.js
@@ -0,0 +1,130 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+
+
+let all_product = [
+  {
+    id: 1,
+    name: "Nike Women's High-Waisted Oversized Tracksuit",
+    category: "women",
+    image: p1_img,
+    new_price: 1199.95,
+  },
+  {
+    id: 2,
+    name: "Nike Women's Mid-Rise Open-Hem Trousers",
+    category: "women",
+    image: p2_img,
+    new_price: 1699.95,
+  },
+  {
+    id: 3,
+    name: "Nike Women's Therma-FIT Loose Hooded Parka",
+    category: "women",
+    image: p3_img,
+    new_price: 2399.97,
+    old_price:  3299.95,
+  },
+  {
+    id: 4,
+    name: "Nike Women's High-Waisted Joggers",
+    category: "women",
+    image: p4_img,
+    new_price: 1099.95,
+  },
+  {
+    id: 5,
+    name: "Nike Women's Golf Dress",
+    category: "women",
+    image: p5_img,
+    new_price: 1799.97,
+    old_price: 1999.95,
+  },
+  {
+    id: 6,
+    name: "Nike Men's Full-Zip Hoodie",
+    category: "men",
+    image: p6_img,
+    new_price: 1699.99,
+    old_price: 2399.95,
+  },
+  {
+    id: 7,
+    name: "Nike Men's Cargo Trousers",
+    category: "men",
+    image: p7_img,
+    new_price: 1099.95,
+  },
+  {
+    id: 8,
+    name: "Nike Men's Dri-FIT Fleece Tennis Hoodie",
+    category: "men",
+    image: p8_img,
+    new_price: 1599.95,
+  },
+  {
+    id: 9,
+    name: "Nike Men's T-Shirt",
+    category: "men",
+    image: p9_img,
+    new_price: 749.95,
+  },
+  {
+    id: 10,
+    name: "Jordan Men's Fleece Pullover Hoodie",
+    category: "men",
+    image: p10_img,
+    new_price: 1499.95,
+  },
+  {
+    id: 11,
+    name: "Nike Older Kids' (Boys') Full-Zip Hoodie",
+    category: "kid",
+    image: p11_img,
+    new_price: 1699.95,
+  },
+  {
+    id: 12,
+    name: "Nike Older Kids' Pullover Hoodie",
+    category: "kid",
+    image: p12_img,
+    new_price: 699.95,
+  },
+  {
+    id: 13,
+    name: "Nike Older Kids' Tracksuit",
+    category: "kid",
+    image: p13_img,
+    new_price: 1199.95,
+  },
+  {
+    id: 14,
+    name: "Nike Older Kids' (Girls') Jacket",
+    category: "kid",
+    image: p14_img,
+    new_price: 1799.97,
+    old_price: 2399.95,
+  },
+  {
+    id: 15,
+    name: "Nike Older Kids' T-Shirt",
+    category: "kid",
+    image: p15_img,
+    new_price: 429.95,
+  },
+];
+
+export default all_product;
